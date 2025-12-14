a2826b71ac2ac91a8418425bf1c091291bb9e6e4
primenumber <prime@kmc.gr.jp>
2025-12-15 06:12:52 +0900

    Implement test for pext_naive

--- src/bitboard/bits.rs
+++ src/bitboard/bits.rs
@@ -40,3 +40,78 @@ impl Pext for u64 {
         pext_naive(*self, mask)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pext() {
+        // Test cases: (value, mask, expected)
+        let table: [(u64, u64, u64); _] = [
+            // mask is zero
+            (
+                0x0000_0000_0000_0000,
+                0x0000_0000_0000_0000,
+                0x0000_0000_0000_0000,
+            ),
+            (
+                0x1234_5678_9ABC_DEF0,
+                0x0000_0000_0000_0000,
+                0x0000_0000_0000_0000,
+            ),
+            (
+                0xFFFF_FFFF_FFFF_FFFF,
+                0x0000_0000_0000_0000,
+                0x0000_0000_0000_0000,
+            ),
+            // mask is all ones
+            (
+                0x0000_0000_0000_0000,
+                0xFFFF_FFFF_FFFF_FFFF,
+                0x0000_0000_0000_0000,
+            ),
+            (
+                0x1234_5678_9ABC_DEF0,
+                0xFFFF_FFFF_FFFF_FFFF,
+                0x1234_5678_9ABC_DEF0,
+            ),
+            (
+                0xFFFF_FFFF_FFFF_FFFF,
+                0xFFFF_FFFF_FFFF_FFFF,
+                0xFFFF_FFFF_FFFF_FFFF,
+            ),
+            // one-hot
+            (0b1010_1010, 0b0000_1000, 0b1),
+            (0b1010_1010, 0b0000_0100, 0b0),
+            (0b1010_1010, 0b0000_0010, 0b1),
+            (0b1010_1010, 0b0000_0001, 0b0),
+            // continuous ones
+            (0b1111_0000, 0b1111_0000, 0b1111),
+            (0b0000_1111, 0b0000_1111, 0b1111),
+            (0b1010_1010, 0b1111_0000, 0b1010),
+            (0b1010_1010, 0b0000_1111, 0b1010),
+            // alternating ones
+            (0b1111_0000, 0b1010_1010, 0b1100),
+            (0b1111_0000, 0b0101_0101, 0b1100),
+            (0b0000_1111, 0b1010_1010, 0b0011),
+            (0b0000_1111, 0b0101_0101, 0b0011),
+            // random values
+            (
+                0x243F_6A88_85A3_08D3,
+                0x1319_8A2E_0370_7344,
+                0x0000_0000_003B_4502,
+            ),
+            (
+                0xA409_3822_299F_31D0,
+                0x082E_FA98_EC4E_6C89,
+                0x0000_0000_0870_33A4,
+            ),
+        ];
+
+        for &(value, mask, expected) in &table {
+            let result = pext_naive(value, mask);
+            assert_eq!(expected, result, "value: {:b}, mask: {:b}", value, mask);
+        }
+    }
+}
